8c929a53c1fb6c0707e942660a53b025c44f766f
kate2001 <kate2001@cs.washington.edu>
2024-08-01 19:36:41 -0700

    hw-squares

---
+++ client/src/FilePicker.tsx
@@ -0,0 +1,44 @@
+import React, { Component, ChangeEvent, MouseEvent } from "react";
+
+
+type FilePickerProps = {
+  // TODO: may want to add some props
+};
+
+
+type FilePickerState = {
+  /** Text in the name text box. */
+  name: string;
+};
+
+
+/** Displays the list of created design files. */
+export class FilePicker extends Component<FilePickerProps, FilePickerState> {
+
+  constructor(props: FilePickerProps) {
+    super(props);
+
+    this.state = {name: ''};
+  }
+
+  render = (): JSX.Element => {
+    // TODO: format list of files as links
+
+    return (<div>
+        <h3>Files</h3>
+        {/* TODO: Render file links & textbox for creating a file here */}
+      </div>);
+  };
+
+  // Updates our record with the file name text being typed in
+  doNameChange = (evt: ChangeEvent<HTMLInputElement>): void => {
+    // TODO: remove this code, implement
+    console.log(evt);
+  };
+
+  // Updates the UI to show the file editor
+  doCreateClick = (_evt: MouseEvent<HTMLButtonElement>): void => {
+    // TODO: implement
+  };
+
+}
